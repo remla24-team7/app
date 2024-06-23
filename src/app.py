d237f2e3929bc03b09f6e1f4bca5cf638a65029a
avilakathara <59567989+avilakathara@users.noreply.github.com>
2024-06-23 21:47:05 +0200

    Fix naming of metric variables in app.py

--- src/app.py
+++ src/app.py
@@ -11,14 +11,14 @@ version_util = VersionUtil()
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
-metrics.info('appv1_frontend_info', 'Application frontend info', version='1.0.0')
-app1_views_counter = prometheus_client.Counter('app1_views_counter', 'Number of times website has been loaded')
-app1_requests_counter = prometheus_client.Counter('app1_requests_counter', 'Number of requests')
-app1_agree_counter = prometheus_client.Counter('app1_agree_counter', 'Number of times users agree with the result')
-app1_disagree_counter = prometheus_client.Counter('app1_disagree_counter', 'Number of times users disagree with the result')
-app1_legitimate_counter = prometheus_client.Counter('app1_legitimate_counter', 'Number of legitimate URLs')
-app1_phishing_counter = prometheus_client.Counter('app1_phishing_counter', 'Number of phishing URLs')
-app1_predict_time_histogram = prometheus_client.Histogram('app1_predict_time_histogram', 'Time taken for prediction')
+metrics.info('appv2_frontend_info', 'Application frontend info', version='1.0.0')
+app2_views_counter = prometheus_client.Counter('app2_views_counter', 'Number of times website has been loaded')
+app2_requests_counter = prometheus_client.Counter('app2_requests_counter', 'Number of requests')
+app2_agree_counter = prometheus_client.Counter('app2_agree_counter', 'Number of times users agree with the result')
+app2_disagree_counter = prometheus_client.Counter('app2_disagree_counter', 'Number of times users disagree with the result')
+app2_legitimate_counter = prometheus_client.Counter('app2_legitimate_counter', 'Number of legitimate URLs')
+app2_phishing_counter = prometheus_client.Counter('app2_phishing_counter', 'Number of phishing URLs')
+app2_predict_time_histogram = prometheus_client.Histogram('app2_predict_time_histogram', 'Time taken for prediction')
 
 
 @app.route("/")
@@ -28,12 +28,12 @@ def index():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    app1_requests_counter.inc()
+    app2_requests_counter.inc()
 
     tic = time.perf_counter()
     model_response = requests.post(f"{model_service}/predict", json=request.json)
     toc = time.perf_counter()
-    app1_predict_time_histogram.observe(toc - tic)
+    app2_predict_time_histogram.observe(toc - tic)
 
     prediction = model_response.json()
     return jsonify(prediction)
@@ -41,20 +41,20 @@ def predict():
 
 @app.route("/version", methods=["GET"])
 def version():
-    app1_views_counter.inc()
+    app2_views_counter.inc()
     return jsonify(version_util.version)
 
 
 @app.route("/agree", methods=["POST"])
 def agree():
-    app1_agree_counter.inc()
+    app2_agree_counter.inc()
     _ = requests.post(f"{model_service}/agree")
     return Response(status=204)
 
 
 @app.route("/disagree", methods=["POST"])
 def disagree():
-    app1_disagree_counter.inc()
+    app2_disagree_counter.inc()
     _ = requests.post(f"{model_service}/disagree")
     return Response(status=204)
 
